5bb12f1b3b48a349d1a35ce0a95141235116b092
unknown <gagankishor101@gmail.com>
2024-10-14 14:02:06 +0530

    new changes

--- src/pages/Home/WizardResult.jsx
+++ src/pages/Home/WizardResult.jsx
@@ -101,119 +101,198 @@ const WizardResult = () => {
   return (
     <>
       <div className="business-check-container">
-  <h2>Overview of the Business Idea Check</h2>
+        <h2>Overview of the Business Idea Check</h2>
 
-  <div className="business-overview">
-    <div className="overview-left">
-      {/* Market Potential */}
-      <div className="overview-item" style={{ marginTop: "50px" }}>
-        <p>Market Potential</p>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: `${data?.marketPotential}%` }}></div>
-        </div>
-        <span>{data?.marketPotential} %</span>
-      </div>
+        <div className="business-overview">
+          <div className="overview-left">
+            {/* Market Potential */}
+            <div
+              className="overview-item"
+              style={{ marginTop: "50px", position: "relative" }}
+            >
+              <p style={{ display: "inline-block", marginRight: "5px" }}>
+                Market Potential{" "}
+                <div className="tooltip">
+                  ?
+                  <span className="tooltip-text">
+                    This is the description of Market Potential. It shows the
+                    potential of the market to grow.
+                  </span>
+                </div>
+              </p>
 
-      {/* Unique Value Proposition */}
-      <div className="overview-item">
-        <p>Unique Value Proposition</p>
-        <div className="progress-bar">
-                <div className="progress" style={{ width: uniqueValueProposition?.finalScore }}></div>
-        </div>
-        <span>{uniqueValueProposition?.finalScore}</span>
-      </div>
+              {/* Tooltip Container */}
 
-      {/* Revenue Model */}
-      <div className="overview-item">
-        <p>Revenue Model</p>
-        <div className="progress-bar">
-                <div className="progress" style={{ width: RevenueModelData?.finalScore }}></div>
-        </div>
+              <div className="progress-bar">
+                <div
+                  className="progress"
+                  style={{ width: `${data?.marketPotential}%` }}
+                ></div>
+              </div>
+              <span>{data?.marketPotential} %</span>
+            </div>
+
+            {/* Unique Value Proposition */}
+            <div className="overview-item">
+              <p>
+                Unique Value Proposition{" "}
+                <div className="tooltip">
+                  ?
+                  <span className="tooltip-text">
+                    This is the description of Market Potential. It shows the
+                    potential of the market to grow.
+                  </span>
+                </div>
+              </p>
+              <div className="progress-bar">
+                <div
+                  className="progress"
+                  style={{ width: uniqueValueProposition?.finalScore }}
+                ></div>
+              </div>
+              <span>{uniqueValueProposition?.finalScore}</span>
+            </div>
+
+            {/* Revenue Model */}
+            <div className="overview-item">
+              <p>
+                Revenue Model
+                <div className="tooltip">
+                  ?
+                  <span className="tooltip-text">
+                    This is the description of Market Potential. It shows the
+                    potential of the market to grow.
+                  </span>
+                </div>
+              </p>
+              <div className="progress-bar">
+                <div
+                  className="progress"
+                  style={{ width: RevenueModelData?.finalScore }}
+                ></div>
+              </div>
               <span>{RevenueModelData?.finalScore}</span>
-      </div>
+            </div>
 
-      {/* Competitive Landscape */}
-      <div className="overview-item">
-        <p>Competitive Landscape</p>
-        <div className="progress-bar">
-                <div className="progress" style={{ width: CompetitiveLandscapeData?.finalScore }}></div>
-        </div>
+            {/* Competitive Landscape */}
+            <div className="overview-item">
+              <p>
+                Competitive Landscape<div className="tooltip">
+                  ?
+                  <span className="tooltip-text">
+                    This is the description of Market Potential. It shows the
+                    potential of the market to grow.
+                  </span>
+                </div>
+              </p>
+              <div className="progress-bar">
+                <div
+                  className="progress"
+                  style={{ width: CompetitiveLandscapeData?.finalScore }}
+                ></div>
+              </div>
               <span>{CompetitiveLandscapeData?.finalScore}</span>
-      </div>
+            </div>
 
-      {/* Team & Resources */}
-      <div className="overview-item">
-        <p>Team & Resources</p>
-        <div className="progress-bar">
-                <div className="progress" style={{ width: `${TeamAndResourcesData}%` }}></div>
-        </div>
+            {/* Team & Resources */}
+            <div className="overview-item">
+              <p>
+                Team & Resources<div className="tooltip">?<span className="tooltip-text">
+                    This is the description of Market Potential. It shows the
+                    potential of the market to grow.
+                  </span>
+                </div>
+              </p>
+              <div className="progress-bar">
+                <div
+                  className="progress"
+                  style={{ width: `${TeamAndResourcesData}%` }}
+                ></div>
+              </div>
               <span>{TeamAndResourcesData}%</span>
-      </div>
-    </div>
-
-    <div className="overview-right">
-      <div className="business-score-circle">
-        <Doughnut data={bardata} options={options} />
-        <div className="doughnut-center">
+            </div>
+          </div>
+          <div className="overview-right">
+            <div className="business-score-circle">
+              <Doughnut data={bardata} options={options} />
+              <div className="doughnut-center">
                 <p className="circle-text">{data?.success_percentage}%</p>
                 <p>Idea Score</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="business-additional-info">
+          <div className="info-item">
+            <strong>Target Market Size</strong>
+            <span>{data?.totalMarketSize} potential users</span>
+          </div>
+          <div className="info-item">
+            <strong>Investment Level</strong>
+            <span>{data?.InvestmentLevelData}</span>
+          </div>
+          <div className="info-item">
+            <strong>Time to Market</strong>
+            <span> {TimeToMarketData ? TimeToMarketData.totalTTM : "0"} </span>
+          </div>
+          <div className="info-item">
+            <strong>Expected ROI</strong>
+            <span>{data?.ExpectedROIData?.split(":")[1]?.trim() || ""}</span>
+          </div>
+          <div className="info-item">
+            <strong>Market Share</strong>
+            <span>{data?.MarketShareData}</span>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div className="business-additional-info">
-    <div className="info-item">
-      <strong>Target Market Size</strong>
-            <span>{data?.totalMarketSize}  potential users</span>
-    </div>
-    <div className="info-item">
-      <strong>Investment Level</strong>
-      <span>Moderate</span>
-    </div>
-    <div className="info-item">
-      <strong>Time to Market</strong>
-            <span> {TimeToMarketData ?TimeToMarketData.totalTTM:'0'} </span>
-    </div>
-    <div className="info-item">
-      <strong>Expected ROI</strong>
-      <span>25% within 12 months</span>
-    </div>
-    <div className="info-item">
-      <strong>Market Share</strong>
-      <span>Optimistic 1.5%</span>
-    </div>
-  </div>
-  </div>
-  <div className="business-check-container">
-  <div className="ai-generated-content">
-    <h3>Your Idea Overview <FaRobot size={28} style={{ color: "var(--main-color)" }} /></h3>
-    {loading ? <p>Loading AI-generated insights...</p> : <p>{displayedText}</p>}
-  </div>
-  </div>
-  <div className="business-check-container">
-  <div className="business-improvements">
-    <h3>How you can improve your idea</h3>
-    <ul>
-      <li className="important">
-        <span><input type="checkbox" /> Clarify the Unique Value Proposition (UVP).</span>
-        <span style={{ color: "orange" }}>Important!</span>
-      </li>
-      <li>
-        <span><input type="checkbox" /> Improve the scalability of the business model.</span>
-        <span>Tip!</span>
-      </li>
-      <li>
-        <span><input type="checkbox" /> Analyze the competitive landscape further.</span>
-        <span>Tip!</span>
-      </li>
-      <li>
-        <span><input type="checkbox" /> Identify additional potential partnerships.</span>
-        <span>Tip!</span>
-      </li>
-    </ul>
-  </div>
-</  div>
-
+      <div className="business-check-container">
+        <div className="ai-generated-content">
+          <h3>
+            Your Idea Overview{" "}
+            <FaRobot size={28} style={{ color: "var(--main-color)" }} />
+          </h3>
+          {loading ? (
+            <p>Loading AI-generated insights...</p>
+          ) : (
+            <p>{displayedText}</p>
+          )}
+        </div>
+      </div>
+      <div className="business-check-container">
+        <div className="business-improvements">
+          <h3>How you can improve your idea</h3>
+          <ul>
+            <li className="important">
+              <span>
+                <input type="checkbox" /> Clarify the Unique Value Proposition
+                (UVP).
+              </span>
+              <span style={{ color: "orange" }}>Important!</span>
+            </li>
+            <li>
+              <span>
+                <input type="checkbox" /> Improve the scalability of the
+                business model.
+              </span>
+              <span>Tip!</span>
+            </li>
+            <li>
+              <span>
+                <input type="checkbox" /> Analyze the competitive landscape
+                further.
+              </span>
+              <span>Tip!</span>
+            </li>
+            <li>
+              <span>
+                <input type="checkbox" /> Identify additional potential
+                partnerships.
+              </span>
+              <span>Tip!</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };

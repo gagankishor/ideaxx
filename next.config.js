115877fcc1accfb3a11ff191e7e2202ad433f7d2
unknown <gagankishor101@gmail.com>
2024-11-02 09:39:34 +0530

    some fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 // next.config.js
 export default {
   reactStrictMode: true,
-  
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // Matches all paths
+        has: [
+          {
+            type: 'host',
+            value: 'www.ideax.in', // Your domain with www
+          },
+        ],
+        destination: 'https://ideax.in/:path*', // Redirects to the non-www version with the same path
+        permanent: true, // Indicates a 301 permanent redirect
+      },
+    ];
+  },
 };

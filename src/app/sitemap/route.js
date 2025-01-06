0c198f60d3754fd39bc2851e281c19dc08658d96
unknown <gagankishor101@gmail.com>
2025-01-06 14:50:03 +0530

    new changes

--- src/app/sitemap/route.js
+++ src/app/sitemap/route.js
@@ -1,13 +1,10 @@
-export async function GET(request) {
-    // Define your static and/or dynamic URLs
+export async function GET() {
     const urls = [
       { loc: 'https://idxsolana.io/', lastmod: '2025-01-01', priority: 1.0 },
       { loc: 'https://idxsolana.io/about', lastmod: '2025-01-01', priority: 0.8 },
       { loc: 'https://idxsolana.io/contact', lastmod: '2025-01-01', priority: 0.5 },
-      // Add more static or dynamically fetched URLs here
     ];
   
-    // Generate XML for the sitemap
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${urls
@@ -23,7 +20,6 @@ export async function GET(request) {
         .join('')}
     </urlset>`;
   
-    // Return the sitemap as an XML response
     return new Response(sitemap, {
       headers: {
         'Content-Type': 'application/xml',

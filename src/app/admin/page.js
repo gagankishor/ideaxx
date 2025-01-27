cb4f37d22750e0d369cdd7cf8fe0b69a345e7111
unknown <gagankishor101@gmail.com>
2025-01-28 00:28:55 +0530

    new changes

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -10,7 +10,7 @@ const AdminPanel = () => {
   // );
 
   return (
-    <div className="mt-8">
+    <div className="p-8">
   <h2 className="text-xl font-bold text-gray-800 mb-4">Welcome to the Admin Panel</h2>
   <p className="text-gray-600">Manage users and their available tests from this dashboard. Use the edit and delete buttons to modify user details or remove users from the system.</p>
 </div>

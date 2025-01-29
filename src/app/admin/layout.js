8f024a654393751b031e517c033ce17f34439794
unknown <gagankishor101@gmail.com>
2025-01-29 18:20:38 +0530

    admin page

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,17 +1,52 @@
-import PropTypes from 'prop-types';
-import AdminSidebar from '@/components/pages/admin/AdminSidebar';
+"use client";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import AdminSidebar from "@/components/pages/admin/AdminSidebar";
+import useAxiosWithAuth from "@/config/useAxiosWithAuth";
 
 const AdminLayout = ({ children }) => {
+  const router = useRouter();
+  const [isAdmin, setIsAdmin] = useState(null);
+  const axiosInstance = useAxiosWithAuth();
+
+  useEffect(() => {
+    const checkAdminPermission = async () => {
+      try {
+        const { data } = await axiosInstance.get("auth/profile");
+        if (data?.role === "admin") {
+          setIsAdmin(true);
+        } else {
+          setIsAdmin(false);
+        }
+      } catch (error) {
+        console.error("Error fetching admin permissions:", error);
+        router.replace("/404");
+      }
+    };
+
+    checkAdminPermission();
+  }, [router, axiosInstance]);
+
+  if (isAdmin === null) {
+    return <div className="min-h-screen flex items-center justify-center">Loading...</div>;
+  }
+  if (isAdmin === false) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center text-center">
+        <h1 className="text-2xl font-bold text-red-500">Access Denied</h1>
+        <p className="text-gray-700 mt-2">You don't have admin access.</p>
+      </div>
+    );
+  }
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Sidebar */}
       <AdminSidebar />
-
       {/* Main Content */}
       <div className="lg:ml-64">
-        {/* Main Content Area */}
         <main className="min-h-screen">
-          <div className="">{children}</div>
+          <div>{children}</div>
         </main>
       </div>
     </div>
